43884eba647f1e65a6afb04e1292e0436987e35a
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-09-30 22:20:02 +0900

    Add files via upload

--- Feed Forward/FeedForward.py
+++ Feed Forward/FeedForward.py
@@ -1,27 +1,77 @@
+from typing_extensions import Required
 import torch
+import torch.nn as nn
+import numpy as np
+class Mymodel(nn.Module):
+    def __init__(self, input_size, output_size):
+        
+        super().__init__()
 
-#선형 계층 구현
+        self.linear = nn.Linear(input_size, output_size)
+    
+    def forward(self, x):
+        y = self.linear(x)
+        return y
 
-# torch.mm == 행렬과 행렬 곱
-# torch.mv == 행렬과 벡터 곱
-# torch.dot == 벡터 내적
-# torch.matmul == 인수 설정으로 mm, mv, dot 가능
-def linear(x, W, b):
-    y = torch.mm(x, W) + b
-    return y
+def ground_truth(x):
+    return 3 * x[:, 0] + x[:, 1] -2 * x[:,2]
 
-x = torch.FloatTensor(16, 10)
-W = torch.FloatTensor(10,5)
-b = torch.FloatTensor(5)
 
-y = linear(x, W, b)
+def train(model, x, y, optim):
+    optim.zero_grad()
 
-y
+    # feed-foward
+    y_hat = model(x)
 
-# x1 = torch.FloatTensor(3,4)
-# x2 = torch.FloatTensor(4,5)
-# torch.mm(x1,x2).size()
+    # get error between answer and inferenced.
+    loss = ((y-y_hat)**2).sum() / x.size(0)
 
-# x1 = torch.FloatTensor(10,3,4)
-# x2 = torch.FloatTensor(10,4,5)
-# torch.bmm(x1,x2).size()
+    # back-propagation
+    loss.backward()
+
+    optim.step()
+
+    return loss.data
+
+
+
+
+batch_size = 1
+n_epochs = 1000
+n_iter = 10000
+
+model = Mymodel(3, 1)
+# model.parameters : dim / lr : learning rate 
+optim = torch.optim.SGD(model.parameters(), lr = 1e-4, momentum = 0.1)
+
+print(model)
+
+# training
+
+for epoch in range(n_epochs):
+    avg_loss = 0 # loss를 계속해서 더해가면서 최소가 되는 지점을 찾는다.
+
+    for i in range(n_iter):
+        x = torch.rand(batch_size, 3) # 1 by 3 where batch_size == 1
+        y = ground_truth(x.data)
+
+        loss = train(model, x, y, optim) # optim : SGD
+
+        avg_loss += loss
+        avg_loss = avg_loss / n_iter
+
+    # simple test sample to check the network.
+    x_valid = torch.FloatTensor([[.3, .2, .1]])
+    y_valid = ground_truth(x_valid.data)
+
+    model.eval()
+
+    y_hat = model(x_valid)
+    model.train()
+
+    print( avg_loss, y_valid.data[0], y_hat.data[0, 0])
+
+    if avg_loss < .001: # finish the training if the loss is smaller than .001 
+        break
+
+
