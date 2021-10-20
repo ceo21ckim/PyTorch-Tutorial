5b775afdf0096c88f3ebca58ce59d791f7c1ab57
KIM DONG EON <60685175+ceo21ckim@users.noreply.github.com>
2021-10-20 22:17:06 +0900

    Add files via upload

--- RNN/basic.py
+++ RNN/basic.py
@@ -50,3 +50,70 @@ rnn = torch.nn.RNN(input_size, hidden_size)
 outputs, _status = rnn(input_data)
 outputs.shape
 
+
+
+# HiHello problem
+
+# one-hot encoding
+
+char_set = ['h', 'i', 'e', 'l', 'o']
+x_data = [[0, 1, 0, 2, 3, 3]]
+x_one_hot = [[[1, 0, 0, 0, 0],
+             [0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0, 0, 1, 0, 0],
+             [0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0]]]
+y_data = [[1, 0, 2, 3, 3, 4]]
+
+# Cross Entropy Loss 
+criterion = torch.nn.CrossEntropyLoss()
+
+input_size = len(char_set)
+hidden_size = len(char_set)
+lr = 0.1
+
+X = torch.Tensor(x_one_hot)
+Y = torch.LongTensor(y_data)
+
+loss = criterion(outputs.view(-1, input_size), Y.view(-1))
+
+
+# charseq
+sample = ' if you want you'
+
+char_set = list(set(sample))
+char_dic = {c : i for i, c in enumerate(char_set)}
+char_dic
+dic_size = len(char_dic)
+input_size = len(char_dic)
+hidden_size = len(char_dic)
+
+sample_idx = [char_dic[c] for c in sample]
+x_data = [sample_idx[:-1]]
+x_one_hot = [np.eye(dic_size)[x] for x in x_data]
+
+y_data = [sample_idx[1:]]
+
+X = torch.Tensor(x_one_hot)
+Y = torch.LongTensor(y_data)
+
+# declare RNN
+# batch_first guarantees the order of output = (B, S, F)
+rnn = torch.nn.RNN(input_size, hidden_size, batch_first = True)
+
+# loss & optimizer setting
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(rnn.parameters(), lr = lr)
+
+for i in range(300):
+    optimizer.zero_grad()
+    # _status 만약 다음 단어가 존재하면 저장해두는 hidden state
+    outputs, _status = rnn(X)
+    loss = criterion(outputs.view(-1, input_size), Y.view(-1))
+    loss.backward()
+    optimizer.step()
+    result = outputs.data.numpy().argmax(axis = 2)
+    result_str = ''.join([char_set[c] for c in np.squeeze(result)])
+    print(i, 'loss :', loss.item(), 'prediction : ', result, 'true Y:', y_data, 'prediction str: ', result_str)
+
